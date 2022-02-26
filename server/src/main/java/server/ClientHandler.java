5b0f13b4c3dfb2930a8124bfb50a8fd1f4a626ef
Yaheni1992 <neo_jeka@mail.ru>
2022-02-26 21:32:08 +0300

    дз7

--- server/src/main/java/server/ClientHandler.java
+++ server/src/main/java/server/ClientHandler.java
@@ -1,13 +1,96 @@
 package server;
 
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 public class ClientHandler {
     private Server server;
     private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    private boolean authenticated;
+    private String nickname;
 
     public ClientHandler(Server server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
 
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+
+                        if (str.startsWith("/")) {
+                            if (str.equals("/end")) {
+                                sendMsg("/end");
+                                break;
+                            }
+
+                            if (str.startsWith("/auth")) {
+                                String[] token = str.split(" ", 3);
+                                if (token.length < 3) {
+                                    continue;
+                                }
+                                String newNick = server.getAuthService()
+                                        .getNicknameByLoginAndPassword(token[1], token[2]);
+                                if (newNick != null) {
+                                    nickname = newNick;
+                                    sendMsg("/auth_ok "+ nickname);
+                                    authenticated = true;
+                                    server.subscribe(this);
+                                    break;
+                                } else {
+                                    sendMsg("Логин или пароль не правильный");
+                                }
+                            }
+                        }
+                    }
+
+                    while (authenticated) {
+                        String str = in.readUTF();
+
+                        if (str.equals("/end")) {
+                            sendMsg("/end");
+                            break;
+                        }
+
+                        server.broadcastMsg(this, str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    server.unsubscribe(this);
+                    System.out.println("Клиент отключился");
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNickname() {
+        return nickname;
     }
 }
